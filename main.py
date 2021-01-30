c403f1603a9b0f6e7eb7eb577b3921b365ebf4a4
bormog <bormog@gmail.com>
2021-01-31 04:27:50 +0700

    Add ability yo use index

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from interpreter import Interpreter
 from lexer import Lexer
 from parser import Parser
 
+import pandas as pd
+
 if __name__ == '__main__':
     text = "create table foobar (primary key foo, bar, buz, foobar);"
     text += "describe foobar;"
     # text += "insert into foobar set foo='foo', bar=2, buz=4, foobar=5;"
-    text += "select 1 + 1 + 2 * 2 + 3 * (1 - 1);"
-    text += "select foo, bar from foobar where foo='foo', bar=2 limit 1;"
+    text += "select 99 + 1;"
+    text += "select bar, foo, buz from foobar where foo=12 limit 3;"
 
     lex = Lexer(text)
     parser = Parser(lex)
@@ -18,4 +20,16 @@ if __name__ == '__main__':
     inter = Interpreter(tree)
     output = inter.do()
     pprint(output)
+    # df = pd.DataFrame({'name': ['Raphael', 'Donatello', 'Raphael'],
+    #                    'mask': ['red', 'purple', 'green'],
+    #                    'weapon': ['sai', 'bo staff', 'ok']})
+    # # df = df.set_index('name')
+    #
+    # s = df
+    # # s = s.loc['Raphael']
+    # # s = s.reset_index()
+    # s = s[s["mask"] == "red"][["mask", "name", "weapon"]]
+    # print(s.index.values)
+    # print(s.values.tolist())
+    # df.to_csv('foo.csv', index_label=df.index.name)
 
--- storage.py
+++ storage.py
@@ -11,40 +11,52 @@ class Table:
 
     def __init__(self, primary_key=None, columns=None):
         if primary_key is not None and columns is not None:
-            self.columns = [primary_key] + columns
-            self.df = pd.DataFrame(columns=self.columns)
+            self.primary_key = primary_key
+            self.columns = columns
+            df = pd.DataFrame(columns=[primary_key] + columns)
+            self.df = df.set_index(primary_key)
         else:
+            self.primary_key = None
             self.columns = []
             self.df = None
 
     @staticmethod
     def load(filepath: str):
         table = Table()
-        table.df = pd.read_csv(filepath)
+        table.df = pd.read_csv(filepath, index_col=0)
+        table.primary_key = table.df.index.name
         table.columns = table.df.columns.to_list()
         return table
 
     def column_exists(self, name: str) -> bool:
-        return name in self.columns
+        return name == self.primary_key or name in self.columns
 
     def save(self, filepath: str) -> None:
-        self.df.to_csv(filepath, index=False)
+        self.df.to_csv(filepath, index_label=self.df.index.name)
 
     def info(self) -> list:
         return self.columns
 
-    def insert(self, row: dict) -> None:
-        self.df = self.df.append(row, ignore_index=True)
+    def insert(self, data: dict) -> None:
+        row = pd.DataFrame(data, columns=[self.primary_key] + self.columns)
+        row = row.set_index(self.df.index.name)
+        self.df = pd.concat([self.df, row])
 
     def select(self, result: list, where: list, limit: int = 0):
+        search = self.df
+        search = search.reset_index()
+
         conditions = None
         for column, value in where:
-            condition = (self.df[column] == value)
+            filter = (search[column] == value)
             if conditions is None:
-                conditions = condition
+                conditions = filter
             else:
-                conditions = conditions & condition
-        search = self.df[conditions][result]
+                conditions = conditions & filter
+
+        if conditions is not None:
+            search = search[conditions][result]
+
         if limit:
             search = search[:limit]
         return search.values.tolist()
@@ -80,7 +92,13 @@ class Storage:
             if not table.column_exists(column):
                 raise StorageException("Column %s not exists in table %s" % (column, name))
             else:
-                row[column] = value
+                row[column] = [value]
+
+        if table.primary_key not in row.keys():
+            raise StorageException("Primary key %s is required" % table.primary_key)
+        if row[table.primary_key][0] in table.df.index.values:
+            raise StorageException("Such primary key %s already exists" % row[table.primary_key][0])
+
         table.insert(row)
         table.save(self._path(name))
 
