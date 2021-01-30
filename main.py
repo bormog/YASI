7d9fcf73094a1092027d5923cca63a1273cc7976
bormog <bormog@gmail.com>
2021-01-30 23:07:11 +0700

    Add Table class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from parser import Parser
 if __name__ == '__main__':
     text = "create table foobar (primary key foo, bar, buz, foobar);"
     text += "describe foobar;"
-    text += "insert into foobar set foo='foo', bar=2, buz=4, foobar=5;"
+    # text += "insert into foobar set foo='foo', bar=2, buz=4, foobar=5;"
     text += "select 1 + 1 + 2 * 2 + 3 * (1 - 1);"
     text += "select foo, bar from foobar where foo='foo', bar=2;"
 
@@ -18,3 +18,4 @@ if __name__ == '__main__':
     inter = Interpreter(tree)
     output = inter.do()
     pprint(output)
+
--- storage.py
+++ storage.py
@@ -7,92 +7,89 @@ class StorageException(Exception):
     pass
 
 
+class Table:
+
+    def __init__(self, primary_key=None, columns=None):
+        if primary_key is not None and columns is not None:
+            self.columns = [primary_key] + columns
+            self.df = pd.DataFrame(columns=self.columns)
+        else:
+            self.columns = []
+            self.df = None
+
+    @staticmethod
+    def load(filepath: str):
+        table = Table()
+        table.df = pd.read_csv(filepath)
+        table.columns = table.df.columns.to_list()
+        return table
+
+    def column_exists(self, name: str) -> bool:
+        return name in self.columns
+
+    def save(self, filepath: str) -> None:
+        self.df.to_csv(filepath, index=False)
+
+    def info(self) -> list:
+        return self.columns
+
+    def insert(self, row: dict) -> None:
+        self.df = self.df.append(row, ignore_index=True)
+
+    def select(self, result: list, where: list, limit: int = 0):
+        conditions = None
+        for column, value in where:
+            condition = (self.df[column] == value)
+            if conditions is None:
+                conditions = condition
+            else:
+                conditions = conditions & condition
+        search = self.df[conditions][result]
+        if limit:
+            search = search[:limit]
+        return search.values.tolist()
+
+
 class Storage:
 
     def __init__(self, working_dir: str = "tables"):
         if not os.path.exists(working_dir):
             raise StorageException("%s working dir is not exists")
         self._working_dir = working_dir
-        self._tables = {}
 
-    def _name(self, table: str) -> str:
-        return "%s.csv" % table
+    def _path(self, name: str) -> str:
+        filename = "%s.csv" % name
+        return os.path.join(self._working_dir, filename)
 
-    def _path(self, table: str) -> str:
-        return os.path.join(self._working_dir, self._name(table))
+    def _exists(self, name: str) -> bool:
+        return os.path.exists(self._path(name))
 
-    def _exists(self, table: str) -> bool:
-        path = self._path(table)
-        return os.path.exists(path)
-
-    def _load(self, table: str) -> pd.DataFrame:
-        if not self._exists(table):
-            raise StorageException("This table %s is not exists" % table)
-        df = pd.read_csv(self._path(table))
-        return self._set(table, df)
-
-    def _set(self, table: str, df: pd.DataFrame) -> pd.DataFrame:
-        self._tables[table] = df
-        return self._get(table)
-
-    def _get(self, table: str) -> pd.DataFrame:
-        return self._tables[table]
-
-    def _save(self, table: str) -> None:
-        self._get(table).to_csv(self._path(table), index=False)
-
-    def _column_exists(self, table: str, column: str) -> bool:
-        columns = self._get(table).columns.to_list()
-        for c in columns:
-            if c == column:
-                return True
-        return False
-
-    def create(self, table: str, primary: str, columns: list) -> bool:
-        if self._exists(table):
+    def create(self, name: str, primary: str, columns: list) -> bool:
+        if self._exists(name):
             return False
-        _columns = [primary] + columns
-        df = pd.DataFrame(columns=_columns)
-        self._set(table, df)
-        self._save(table)
-        return self._exists(table)
+        Table(primary, columns).save(self._path(name))
+        return self._exists(name)
 
-    def describe(self, table: str) -> list:
-        self._load(table)
-        return self._get(table).columns.to_list()
+    def describe(self, name: str) -> list:
+        return Table.load(self._path(name)).columns
 
-    def insert(self, table: str, columns: list) -> None:
-        self._load(table)
-
+    def insert(self, name: str, columns: list) -> None:
+        table = Table.load(self._path(name))
         row = {}
         for column, value in columns:
-            if not self._column_exists(table, column):
-                raise StorageException("Column %s not exists in table %s" % (column, table))
+            if not table.column_exists(column):
+                raise StorageException("Column %s not exists in table %s" % (column, name))
             else:
                 row[column] = value
+        table.insert(row)
+        table.save(self._path(name))
 
-        df = self._get(table).append(row, ignore_index=True)
-        self._set(table, df)
-        self._save(table)
-
-    def select(self, table: str, result: list, where: list, limit: int = None) -> list:
-        self._load(table)
-        df = self._get(table)
-
+    def select(self, name: str, result: list, where: list, limit: int = None) -> list:
+        table = Table.load(self._path(name))
         for column in result:
-            if not self._column_exists(table, column):
-                raise StorageException("Column %s not exists in table %s" % (column, table))
-
-        conditions = None
-        for column, value in where:
-            if not self._column_exists(table, column):
-                raise StorageException("Column %s not exists in table %s" % (column, table))
-            condition = (df[column] == value)
-            if conditions is None:
-                conditions = condition
-            else:
-                conditions = condition & condition
-        search = df[conditions][result]
-        if limit:
-            search = search[:limit]
-        return search.values.tolist()
+            if not table.column_exists(column):
+                raise StorageException("Column %s not exists in table %s" % (column, name))
+        for column, _ in where:
+            if not table.column_exists(column):
+                raise StorageException("Column %s not exists in table %s" % (column, name))
+        return table.select(result, where, limit)
